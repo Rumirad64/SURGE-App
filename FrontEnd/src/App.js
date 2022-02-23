b00775682acfbd08e07db08a9a60d6e77df20be8
Rumira Daksith <rumiradaksith123@gmail.com>
2022-02-24 00:06:36 +0530

    Implemented routes
    Used HashRouter because of NGINX

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,12 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h2>Hello World</h2>
-    </div>
-  );
+import {
+	BrowserRouter,
+	HashRouter,
+	Routes,
+	Route,
+	Link
+} from "react-router-dom";
+
+import React from 'react';
+import NavBar from './components/navbar';
+
+import Login from './pages/login';
+import Register from './pages/register';
+import Home from './pages/home';
+import MyAccount from './pages/myaccount';
+
+//! because of NGINX i am using hashrouter. Then does not need nginx further configurations.
+
+function App(props) {
+	return (
+		<>
+			<NavBar />
+			<HashRouter>
+				<Routes>
+					<Route exact path="/" element={<Home />} />
+					<Route path="/myaccount" element={<MyAccount />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/register" element={<Register />} />
+					
+				</Routes>
+			</HashRouter>
+		</>
+	);
 }
 
 export default App;
